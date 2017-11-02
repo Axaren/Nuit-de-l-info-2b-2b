dcf8b87be79d3af2cd9ee10cd26f1b1fa3e47699
Lesne Nathan <nathan.lesne@etu.u-bordeaux.fr>
2017-11-02 06:59:24 +0100

    Amélioration des logs et ajout du dossier node_modules au .gitignore

--- bot.js
+++ bot.js
@@ -4,7 +4,8 @@ var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-    colorize: true
+    colorize: true,
+    timestamp: true
 });
 logger.level = 'debug';
 
@@ -26,51 +27,55 @@ bot.on('ready', function (evt) {
     });
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
+    var serverID = bot.channels[channelID].guild_id;
+    var serverName = bot.servers[serverID].name;
+    var channelName = bot.channels[channelID].name;
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
+    if (message.substring(0, 1) === '!') {
+    logger.info(user + ' on ' + serverName + ': ' + channelName + ' posted ' + message);
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
 
-            case 'author':
-                if (userID == )
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Woof <@' + userID + '> , BuddyBot was written by **Axeran** for *Hewpie Sub Discord*'
-                });
-                break;
+    args = args.splice(1);
+    switch (cmd) {
 
-            case 'invite':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Woof **' + user + '**! \n To invite me on your server click this link: \n https://discordapp.com/oauth2/authorize?client_id=375414998916399104&scope=bot'
-                });
-                break;
-
-            case 'hello':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'hi <@' + userID + '> !'
-                });
+        case 'author':
+            var author = 'Axeran#9861';
+            bot.sendMessage({
+                to: channelID,
+                message: 'Woof <@' + userID + '> , BuddyBot was written by **' + author + '** for *Hewpie Sub Discord*. \n You can find the source code here: https://github.com/Axaren/BuddyBot'
+            });
             break;
 
-            case 'rules':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'KEEP HEWPIE HAPPY'
-                });
+        case 'invite':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Woof **' + user + '**! \n To invite me on your server click this link: \n https://discordapp.com/oauth2/authorize?client_id=375414998916399104&scope=bot'
+            });
             break;
 
-            case 'bark':
-                var reciever = args[0];
-                bot.sendMessage({
-                    to: channelID,
-                    message: '<@' + userID + '> barked at ' + reciever
-                });
+        case 'hello':
+            bot.sendMessage({
+                to: channelID,
+                message: 'hi <@' + userID + '> !'
+            });
             break;
-         }
-     }
+
+        case 'rules':
+            bot.sendMessage({
+                to: channelID,
+                message: 'KEEP HEWPIE HAPPY'
+            });
+            break;
+
+        case 'bark':
+            var reciever = args[0];
+            bot.sendMessage({
+                to: channelID,
+                message: '<@' + userID + '> barked at ' + reciever
+            });
+            break;
+    }
+}
 });
